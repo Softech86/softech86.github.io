2e6e55beea834c5f1d05dfbdadefd46c7ea3841d
softech86 <softech86@163.com>
2017-08-25 17:38:47 +0800

    auto push

--- server/nuxt.config.js
+++ server/nuxt.config.js
@@ -25,15 +25,15 @@ module.exports = {
     ** Run ESLINT on save
     */
     vendor: ['axios'],
-    // extend (config, ctx) {
-    //   if (ctx.dev && ctx.isClient) {
-    //     config.module.rules.push({
-    //       enforce: 'pre',
-    //       test: /\.(js|vue)$/,
-    //       loader: 'eslint-loader',
-    //       exclude: /(node_modules)/
-    //     })
-    //   }
-    // }
+    extend (config, ctx) {
+      if (ctx.dev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
   }
 }
